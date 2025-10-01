0bfea1e75e3bf68abdfe47f520b150b8c0395a83
yanhuang2486 <yanhuang248655@163.com>
2025-10-01 14:44:31 +0800

    On branch master Initial commit Changes to be committed: 	new file:   item.py 	new file:   itemBase.py 	new file:   itemBase.xlsx Changes not staged for commit: 	modified:   itemBase.py  Untracked files: 	reception.py

---
+++ item.py
@@ -0,0 +1,13 @@
+
+class item():
+    def __init__(self, name, price, description, contact):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.contact = contact
+
+    def check_init():
+
+        return
+    
+     
---
+++ itemBase.py
@@ -0,0 +1,70 @@
+import pandas as pd
+
+
+class itemBase():
+
+    def __init__(self):
+        self.items = pd.read_excel('itemBase.xlsx')
+        self.itemNum = len(self.items)
+        pass
+
+    #物品的添加
+    def addItem(self, itemName, itemPrice, 
+                itemDescription, itemOwner, itemOwnerContact):
+        
+        self.itemNum += 1
+        itemTmp = [itemName, itemPrice, itemDescription, itemOwner, itemOwnerContact]
+        self.items.append(itemTmp)
+        
+        return
+    
+    #物品的搜索
+    def searchItem(self, keywords):
+        
+        #判断仓库是否为空
+        if self.itemNum == 0:
+            return 'Empty!'
+        
+        searchTmp=[]
+
+        #在物品名中搜索
+        for i in range(self.itemNum):
+            if keywords in self.items['Name'][i]:
+                searchTmp.append(i)
+        
+        #在描述中搜索
+        for i in range(self.itemNum):
+            if keywords in self.items['Description'][i]:
+                searchTmp.append(i)
+        
+        searchTmp=list(set(searchTmp))
+
+        if len(searchTmp) == 0:
+            return 'None!'
+        else:
+            return searchTmp
+    
+    #物品的删除
+    def deleteItem(self, itemID):
+        
+        self.items.drop(itemID)
+        
+        return
+    
+    #展示所有物品
+    def showAllItems(self):
+        
+        print(self.items['Name', 'Owner'])
+
+        return 
+
+    #程序运行结束，保存
+    def closeAndSave(self):
+
+        self.items.to_excel('itemBase.xlsx')
+        del self.items
+        del self.itemNum
+
+        return
+
+    
